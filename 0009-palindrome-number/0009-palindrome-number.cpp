c12aebac403f119956d08a95d42d0c4b8eeecfcd
Shristi Badhwar <shristibadhwar@gmail.com>
2023-06-19 23:41:56 +0530

    Time: 20 ms (36.97%), Space: 5.8 MB (85.27%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0 || (x!=0 && x%10==0))
+         return false;
+        int half=0;
+        while(x>half)
+        {
+            half=half*10+(x%10);
+            x/=10;
+        } 
+        if(x==half || x==half/10)
+         return true;
+        else
+         return false; 
+        
+    }
+};
