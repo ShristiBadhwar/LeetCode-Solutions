8b9e03bc49457417ba964e3e372a826639a628a6
Shristi Badhwar <shristibadhwar@gmail.com>
2023-06-27 23:31:38 +0530

    Time: 0 ms (100%), Space: 6 MB (48.04%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    int alternate(int n)
+    {
+        if(n<4)
+         return 0;
+        else
+        {
+            int count=0;
+            while(n>1)
+            {
+                count+=n/5;
+                n/=5;
+            }
+            return count;
+        } 
+    }
+public:
+    int trailingZeroes(int n) {
+        return alternate(n);
+        if(n<4)
+         return 0;
+        else
+        {
+            int ans=0;
+            int i=1;
+            while(pow(5,i)<=n)
+            {
+                ans+=n/pow(5,i);
+                i++;
+            }
+            return ans;
+        } 
+        
+    }
+};
