569e72705c8f70a6ac678177709d1ca234f1af9a
Shristi Badhwar <shristibadhwar@gmail.com>
2023-07-04 20:06:02 +0530

    Time: 138 ms (60.31%), Space: 36 MB (8.72%) - LeetHub

--- 2722-prime-in-diagonal/2722-prime-in-diagonal.cpp
+++ 2722-prime-in-diagonal/2722-prime-in-diagonal.cpp
@@ -5,7 +5,7 @@ public:
         if(n==0 || n==1)
         return false;
         bool flag=true;
-        for(int i=2; i<=n/2; i++)
+        for(int i=2; i*i<=(n); i++)
         {
             if(n%i==0)
              return false;
