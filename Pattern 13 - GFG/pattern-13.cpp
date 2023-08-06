64e3a67d1d9e497a3cfd104380978f3da09d263f
Shristi Badhwar <shristibadhwar@gmail.com>
2023-08-06 16:05:33 +0530

    Added solution - LeetHub

---
+++ Pattern 13 - GFG/pattern-13.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int k=1;
+       for(int r=1; r<=n; r++)
+        {
+            for(int c=1; c<=r; c++)
+            {
+                cout<<k<<" ";
+                k++;
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
