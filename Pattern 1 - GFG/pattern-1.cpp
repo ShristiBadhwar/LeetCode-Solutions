5b7c00606ac6c79b6ec92100afb1726e736ac300
Shristi Badhwar <shristibadhwar@gmail.com>
2023-08-06 15:39:47 +0530

    Added solution - LeetHub

---
+++ Pattern 1 - GFG/pattern-1.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+        for(int r=1; r<=n; r++)
+        {
+            for(int c=1; c<=n; c++)
+                cout<<"* ";
+            cout<<endl;    
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
